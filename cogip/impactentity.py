d1320911d52d9735a2413b716dc96eca36da1128
Eric Courtois <eric.courtois@gmail.com>
2020-08-18 15:30:15 +0200

    Parameterize ImpactEntity
    Add possibility to change radius and color of ImpactEntity.
    
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- cogip/impactentity.py
+++ cogip/impactentity.py
@@ -5,15 +5,15 @@ from PySide2.Qt3DExtras import Qt3DExtras
 
 class ImpactEntity(Qt3DCore.QEntity):
 
-    def __init__(self):
+    def __init__(self, radius: float = 50, color: QtCore.Qt.GlobalColor = QtCore.Qt.red):
         super(ImpactEntity, self).__init__()
 
         self.mesh = Qt3DExtras.QSphereMesh()
-        self.mesh.setRadius(50)
+        self.mesh.setRadius(radius)
         self.addComponent(self.mesh)
 
         self.material = Qt3DExtras.QPhongMaterial()
-        self.material.setDiffuse(QtGui.QColor(QtCore.Qt.red))
+        self.material.setDiffuse(QtGui.QColor(color))
         self.addComponent(self.material)
 
         self.transform = Qt3DCore.QTransform()
