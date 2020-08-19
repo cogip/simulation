e40ccdc421a393ff327f8c24f0dc0439f8f792a6
Eric Courtois <eric.courtois@gmail.com>
2020-08-19 15:04:36 +0200

    Add LIDAR sensors
    Derived Sensor class to specialize ToFSensor and LidarSensor.
    Change default obstacle height to 600 to be detected by the Lidar.
    Change PickMethod to TrianglePicking for better performance.
    Create a QLayer containing the entity to be detected and add it to the ray casters
    to avoid detecting the table, robot, impact entities.
    
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- cogip/sensor.py
+++ cogip/sensor.py
@@ -1,4 +1,5 @@
 from PySide2 import QtCore, QtGui
+from PySide2.Qt3DCore import Qt3DCore
 from PySide2.Qt3DRender import Qt3DRender
 from PySide2.QtCore import Slot as qtSlot
 
@@ -8,31 +9,46 @@ from cogip.impactentity import ImpactEntity
 
 class Sensor(QtCore.QObject):
 
+    # Class attribute recording all entities that should be detected
+    obstacles = []
+
+    # Class attribute recording all sensors
+    # Each added obstacle must be registered in all sensors
+    all_sensors = []
+
     def __init__(
             self,
             asset_entity: AssetEntity,
             name: str,
             origin_x: int,
             origin_y: int,
+            origin_z: int,
+            direction_x: int,
+            direction_y: int,
+            direction_z: int,
             impact_radius: float = 50,
-            impact_color: QtCore.Qt.red = QtCore.Qt.red):
+            impact_color: QtCore.Qt.GlobalColor = QtCore.Qt.red):
         super(Sensor, self).__init__()
+
+        Sensor.all_sensors.append(self)
 
         self.asset_entity = asset_entity
         self.name = name
         self.distance = None
 
-        direction_x = 1.0 if origin_x > 0 else -1.0 if origin_x < 0 else 0.0
-        direction_y = 1.0 if origin_y > 0 else -1.0 if origin_y < 0 else 0.0
-
         self.ray_caster = Qt3DRender.QRayCaster()
-        self.ray_caster.setEnabled(True)
+        self.ray_caster.setEnabled(False)  # Start casting only when the first obstacle is registered
         self.ray_caster.setLength(0)  # Infinite
         self.ray_caster.setRunMode(Qt3DRender.QAbstractRayCaster.Continuous)
-        self.ray_caster.setOrigin(QtGui.QVector3D(float(origin_x), float(origin_y), 60))
-        self.ray_caster.setDirection(QtGui.QVector3D(direction_x, direction_y, 0))
-
+        # self.ray_caster.setRunMode(Qt3DRender.QAbstractRayCaster.SingleShot)
+        self.ray_caster.setFilterMode(Qt3DRender.QAbstractRayCaster.AcceptAnyMatchingLayers)
+        self.ray_caster.setOrigin(QtGui.QVector3D(float(origin_x), float(origin_y), float(origin_z)))
+        self.ray_caster.setDirection(QtGui.QVector3D(direction_x, direction_y, direction_z))
         self.asset_entity.asset_entity.addComponent(self.ray_caster)
+
+        # Add layers for obstacles already present
+        for obstacle in self.obstacles:
+            self.add_obstacle_layer(obstacle)
 
         # Add impact entity
         self.impact_entity = ImpactEntity(radius=impact_radius, color=impact_color)
@@ -47,7 +63,7 @@ class Sensor(QtCore.QObject):
         distances = [
             hit
             for hit in self.ray_caster.hits()
-            if hit.distance() != 0.0 and hit.entity() != self.impact_entity
+            if hit.distance() != 0.0
         ]
         if len(distances):
             self.hit = min(distances, key=lambda x: x.distance())
@@ -56,3 +72,68 @@ class Sensor(QtCore.QObject):
         else:
             self.hit = None
             self.impact_entity.setEnabled(False)
+
+    @classmethod
+    def add_obstacle(cls, obstacle: Qt3DCore.QEntity):
+        cls.obstacles.append(obstacle)
+        for sensor in cls.all_sensors:
+            sensor.add_obstacle_layer(obstacle)
+
+    # Create a QLayer containing the entity to be detected
+    # and add it to the ray caster
+    def add_obstacle_layer(self, obstacle: Qt3DCore.QEntity):
+        # It may be possible to reuse the same layer
+        # if it has arlready been added to the obstacle
+        layer = Qt3DRender.QLayer(obstacle)
+        layer.setRecursive(True)
+        layer.setEnabled(True)
+        obstacle.addComponent(layer)
+        self.ray_caster.addLayer(layer)
+        # Activate if not already done
+        self.ray_caster.trigger()
+
+
+class ToFSensor(Sensor):
+
+    def __init__(
+            self,
+            asset_entity: AssetEntity,
+            name: str,
+            origin_x: int,
+            origin_y: int):
+
+        super(ToFSensor, self).__init__(
+            asset_entity=asset_entity,
+            name=name,
+            origin_x=origin_x,
+            origin_y=origin_y,
+            origin_z=60,
+            direction_x=origin_x,
+            direction_y=origin_y,
+            direction_z=0,
+            impact_radius=50,
+            impact_color=QtCore.Qt.red)
+
+
+class LidarSensor(Sensor):
+
+    def __init__(
+            self,
+            asset_entity: AssetEntity,
+            name: str,
+            origin_x: int,
+            origin_y: int,
+            direction_x: int,
+            direction_y: int):
+
+        super(LidarSensor, self).__init__(
+            asset_entity=asset_entity,
+            name=name,
+            origin_x=origin_x,
+            origin_y=origin_y,
+            origin_z=400,
+            direction_x=direction_x,
+            direction_y=direction_y,
+            direction_z=0,
+            impact_radius=20,
+            impact_color=QtCore.Qt.blue)
