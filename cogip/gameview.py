bf4b5f47206f7cb6e3b0ecabb086f41376c1baa6
Eric Courtois <eric.courtois@gmail.com>
2020-12-04 18:00:38 +0100

    gameview: cleanup and reduce min size
    - Remove commented out code
    - Reduce minimum size
    - Zoom out to fit in the widget

--- cogip/gameview.py
+++ cogip/gameview.py
@@ -51,19 +51,15 @@ class GameView(QtWidgets.QWidget):
 
         # Create the view and set it as the widget layout
         self.view = Qt3DExtras.Qt3DWindow()
-        screen_size = self.view.screen().size()
         self.container = self.createWindowContainer(self.view)
-        self.container.setMinimumSize(QtCore.QSize(800, 600))
-        self.container.setMaximumSize(screen_size)
+        self.container.setMinimumSize(QtCore.QSize(400, 400))
         self.container.setFocusPolicy(QtCore.Qt.NoFocus)
         layout = QtWidgets.QHBoxLayout()
         layout.addWidget(self.container)
         self.setLayout(layout)
 
         picking_settings = self.view.renderSettings().pickingSettings()
-        # picking_settings.setPickMethod(Qt3DRender.QPickingSettings.PrimitivePicking)
         picking_settings.setPickMethod(Qt3DRender.QPickingSettings.TrianglePicking)
-        # picking_settings.setPickResultMode(Qt3DRender.QPickingSettings.NearestPick)
         picking_settings.setPickResultMode(Qt3DRender.QPickingSettings.AllPicks)
 
         # Create root entity
@@ -71,19 +67,16 @@ class GameView(QtWidgets.QWidget):
         self.view.setRootEntity(self.root_entity)
 
         # Init Camera
-        self.camera_entity = self.view.camera()  # type: Qt3DRender.QCamera
-        # QCameraLens::setPerspectiveProjection(float fieldOfView, float aspectRatio, float nearPlane, float farPlane)
+        self.camera_entity: Qt3DRender.QCamera = self.view.camera()
         self.camera_entity.lens().setPerspectiveProjection(45.0, 16.0 / 9.0, 0.1, 20000.0)
         self.camera_entity.setUpVector(QtGui.QVector3D(0, 0, 1))
-        self.camera_entity.setPosition(QtGui.QVector3D(3000, 3000, 3000))
+        self.camera_entity.setPosition(QtGui.QVector3D(4000, 4000, 4000))
         self.camera_entity.setViewCenter(QtGui.QVector3D(0, 1000, 0))
 
         # Create camera controller entity
         self.camera_controller = Qt3DExtras.QOrbitCameraController(self.root_entity)
-        # self.camera_controller = Qt3DExtras.QFirstPersonCameraController(self.root_entity)
         self.camera_controller.setCamera(self.camera_entity)
         self.camera_controller.setLinearSpeed(10000)  # default = 10
-        # self.camera_controller.setLookSpeed(1000)  # default = 180
 
         # Create light
         self.light_entity = create_ligth_entity(20000, 20000, 20000)
