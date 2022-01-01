e68fdc5e679aaf5f120f2f759eab6a5d76508654
Eric Courtois <eric.courtois@gmail.com>
2022-01-01 23:35:01 +0100

    entities: table: remove 2021 buoys
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- cogip/entities/table.py
+++ cogip/entities/table.py
@@ -1,54 +1,11 @@
 from pathlib import Path
 from typing import Optional
 
-from PySide2 import QtCore, QtGui
 from PySide2.Qt3DCore import Qt3DCore
 from PySide2.Qt3DExtras import Qt3DExtras
 
 from cogip import logger
 from cogip.entities.asset import AssetEntity
-
-
-buoys_colors = {
-    "green": QtGui.QColor(QtCore.Qt.darkGreen),
-    "red": QtGui.QColor(QtCore.Qt.red)
-}
-
-buoys = [
-    # Blue side (+X)
-    {"x": 1500-300, "y": 400, "color": buoys_colors["red"]},
-    {"x": 1500-300, "y": 1200, "color": buoys_colors["green"]},
-    {"x": 1500-445, "y": 515, "color": buoys_colors["green"]},
-    {"x": 1500-445, "y": 1085, "color": buoys_colors["red"]},
-    {"x": 1500-670, "y": 100, "color": buoys_colors["red"]},
-    {"x": 1500-956, "y": 400, "color": buoys_colors["green"]},
-    {"x": 1500-1005, "y": 1955, "color": buoys_colors["red"]},
-    {"x": 1500-1065, "y": 1655, "color": buoys_colors["green"]},
-    {"x": 1500-1100, "y": 800, "color": buoys_colors["red"]},
-    {"x": 1500-1270, "y": 1200, "color": buoys_colors["green"]},
-    {"x": 1500-1335, "y": 1655, "color": buoys_colors["red"]},
-    {"x": 1500-1395, "y": 1955, "color": buoys_colors["green"]},
-    # Yellow side (-X)
-    {"x": 1500-1605, "y": 1955, "color": buoys_colors["red"]},
-    {"x": 1500-1665, "y": 1655, "color": buoys_colors["green"]},
-    {"x": 1500-1730, "y": 1200, "color": buoys_colors["red"]},
-    {"x": 1500-1900, "y": 800, "color": buoys_colors["green"]},
-    {"x": 1500-1935, "y": 1655, "color": buoys_colors["red"]},
-    {"x": 1500-1995, "y": 1955, "color": buoys_colors["green"]},
-    {"x": 1500-2044, "y": 400, "color": buoys_colors["red"]},
-    {"x": 1500-2330, "y": 100, "color": buoys_colors["green"]},
-    {"x": 1500-2555, "y": 515, "color": buoys_colors["red"]},
-    {"x": 1500-2555, "y": 1085, "color": buoys_colors["green"]},
-    {"x": 1500-2700, "y": 400, "color": buoys_colors["green"]},
-    {"x": 1500-2700, "y": 1200, "color": buoys_colors["red"]},
-    # Shoal area (floating buoys)
-    {"x": 1500-1200, "y": 75, "color": buoys_colors["green"]},
-    {"x": 1500-1340, "y": 330, "color": buoys_colors["green"]},
-    {"x": 1500-1360, "y": 125, "color": buoys_colors["red"]},
-    {"x": 1500-1660, "y": 400, "color": buoys_colors["red"]},
-    {"x": 1500-1700, "y": 100, "color": buoys_colors["red"]},
-    {"x": 1500-1900, "y": 200, "color": buoys_colors["green"]}
-]
 
 
 class TableEntity(AssetEntity):
@@ -74,7 +31,6 @@ class TableEntity(AssetEntity):
         Inherits [AssetEntity][cogip.entities.asset.AssetEntity].
         """
         super().__init__(self.asset_path, scale=self.asset_scale, parent=parent)
-        self.buoy_entities = []
 
     def post_init(self):
         """
@@ -104,23 +60,3 @@ class TableEntity(AssetEntity):
                 if isinstance(comp, Qt3DExtras.QPhongMaterial):
                     self.ground_entity.removeComponent(comp)
                     break
-
-        for buoy in buoys:
-            entity = Qt3DCore.QEntity(self)
-            self.buoy_entities.append(entity)
-
-            mesh = Qt3DExtras.QConeMesh(entity)
-            mesh.setBottomRadius(72/2)
-            mesh.setTopRadius(54/2)
-            mesh.setLength(115)
-            mesh.setHasTopEndcap(True)
-            entity.addComponent(mesh)
-
-            material = Qt3DExtras.QPhongMaterial(entity)
-            material.setDiffuse(QtGui.QColor(buoy["color"]))
-            entity.addComponent(material)
-
-            transform = Qt3DCore.QTransform(entity)
-            transform.setTranslation(QtGui.QVector3D(buoy["x"], buoy["y"], 115.0/2))
-            transform.setRotationX(90)
-            entity.addComponent(transform)
