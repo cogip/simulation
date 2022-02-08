14194a233b35f7fb3cf34d6923953f1e4bfda387
Eric Courtois <eric.courtois@gmail.com>
2022-02-08 10:43:47 +0100

    tools: monitor: add a robot entity that can be positioned manually
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

---
+++ cogip/entities/robot_manual.py
@@ -0,0 +1,271 @@
+from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.Qt3DCore import Qt3DCore
+from PySide6.Qt3DRender import Qt3DRender
+from PySide6.Qt3DExtras import Qt3DExtras
+from PySide6.QtCore import Signal as qtSignal
+from PySide6.QtCore import Slot as qtSlot
+
+
+class RobotManualEntity(Qt3DCore.QEntity):
+    """
+    An robot entity that can be moved and rotated manually on the table.
+
+    When selected with a mouse click, a property window is displayed
+    to modify the robot properties.
+
+    The robot can also be moved using the mouse.
+
+    Attributes:
+        color: Robot color
+        enable_controller: Qt signal used to disable the camera controller
+            when moving the robot using the mouse
+    """
+
+    color: QtGui.QColor = QtGui.QColor.fromRgb(0, 102, 204, 100)
+    enable_controller = qtSignal(bool)
+
+    def __init__(
+            self,
+            parent: Qt3DCore.QEntity,
+            parent_widget: QtWidgets.QWidget,
+            x: int = 0,
+            y: int = 1000,
+            rotation: int = 0):
+        """
+        Class constructor.
+
+        Arguments:
+            parent: The parent entity
+            parent_widget: Parent widget
+            x: X position
+            y: Y position
+            rotation: Rotation
+        """
+        super().__init__(parent)
+
+        self.parent_widget = parent_widget
+
+        self.mesh = Qt3DRender.QMesh(self)
+        self.mesh.setSource(QtCore.QUrl("file:assets/robot2022.obj"))
+        self.addComponent(self.mesh)
+
+        self.material = Qt3DExtras.QPhongMaterial(self)
+        self.material.setShininess(1.0)
+        self.material.setDiffuse(self.color)
+        self.material.setSpecular(self.color)
+        self.addComponent(self.material)
+
+        self.transform = Qt3DCore.QTransform(self)
+        self.transform.setTranslation(QtGui.QVector3D(x, y, 0))
+        self.transform.setRotationZ(rotation)
+        self.addComponent(self.transform)
+
+        self.picker = Qt3DRender.QObjectPicker()
+        self.picker.setDragEnabled(True)
+        self.picker.pressed.connect(self.pressed)
+        self.picker.released.connect(self.released)
+        self.picker.moved.connect(self.moved)
+        self.addComponent(self.picker)
+
+        self.moving = False
+
+        # Create a layer used by sensors to activate detection on the robot
+        self.layer = Qt3DRender.QLayer(self)
+        self.layer.setRecursive(True)
+        self.layer.setEnabled(True)
+        self.addComponent(self.layer)
+
+        # Create properties dialog
+        self.properties = RobotManualProperties(self.parent_widget, self)
+
+    @qtSlot(int)
+    def setXTranslation(self, x: int):
+        """
+        Qt Slot
+
+        Set the X position.
+        """
+        translation = self.transform.translation()
+        translation.setX(float(x))
+        self.transform.setTranslation(translation)
+
+    @qtSlot(int)
+    def setYTranslation(self, y: int):
+        """
+        Qt Slot
+
+        Set the Y position.
+        """
+        translation = self.transform.translation()
+        translation.setY(float(y))
+        self.transform.setTranslation(translation)
+
+    @qtSlot(Qt3DRender.QPickEvent)
+    def pressed(self, pick: Qt3DRender.QPickEvent):
+        """
+        Qt Slot
+
+        Slot called on a ```pressed``` mouse event on the robot.
+
+        Emit a signal to disable the camera controller before moving the robot.
+        """
+        self.enable_controller.emit(False)
+
+    @qtSlot(Qt3DRender.QPickEvent)
+    def released(self, pick: Qt3DRender.QPickEvent):
+        """
+        Qt Slot
+
+        Slot called on a ```released``` mouse event on the robot.
+
+        If this event occurs juste after a ```pressed``` event, it is only a mouse click,
+        so display the property window.
+
+        Emit a signal to re-enable the camera controller after moving the robot.
+        """
+        if not self.moving:
+            if RobotManualProperties.active_properties:
+                RobotManualProperties.active_properties.close()
+            self.properties.restore_saved_geometry()
+            self.properties.show()
+            self.properties.raise_()
+            self.properties.activateWindow()
+            RobotManualProperties.set_active_properties(self.properties)
+        self.moving = False
+        self.enable_controller.emit(True)
+
+    @qtSlot(Qt3DRender.QPickEvent)
+    def moved(self, pick: Qt3DRender.QPickEvent):
+        """
+        Qt Slot
+
+        Slot called on a ```moved``` mouse event on the robot.
+
+        Just record that the robot is moving, the translation is computed
+        in the [GameView][cogip.widgets.gameview.GameView] object.
+        """
+        self.moving = True
+
+    @qtSlot(QtGui.QVector3D)
+    def new_move_delta(self, delta: QtGui.QVector3D):
+        """
+        Qt Slot
+
+        Update the robot position.
+
+        Arguments:
+            delta: The difference between current and new position
+        """
+        self.move_delta = delta
+        if self.moving and delta:
+            new_translation = self.transform.translation() + delta
+            self.transform.setTranslation(new_translation)
+            self.properties.spin_x.setValue(new_translation.x())
+            self.properties.spin_y.setValue(new_translation.y())
+
+
+class RobotManualProperties(QtWidgets.QDialog):
+    """
+    The property window.
+
+    Attributes:
+        saved_geometry: The position of the last displayed property window
+        active_geometry: The current property window displayed.
+    """
+    saved_geometry: QtCore.QRect = None
+    active_properties: "RobotManualProperties" = None
+
+    def __init__(self, parent: QtWidgets.QWidget, robot_entity: RobotManualEntity):
+        """
+        Class constructor.
+
+        Arguments:
+            parent: The parent widget
+            robot_entity: The related robot entity
+        """
+        super().__init__(parent)
+
+        self.robot_entity = robot_entity
+        self.setWindowTitle("Robot Properties")
+        self.setModal(False)
+        self.setMinimumWidth(self.fontMetrics().horizontalAdvance(self.windowTitle()))
+
+        layout = QtWidgets.QGridLayout()
+        self.setLayout(layout)
+
+        row = 0
+
+        label_x = QtWidgets.QLabel("X")
+        self.spin_x = QtWidgets.QSpinBox()
+        self.spin_x.setStepType(QtWidgets.QAbstractSpinBox.AdaptiveDecimalStepType)
+        self.spin_x.setMinimum(-1500)
+        self.spin_x.setMaximum(1500)
+        self.spin_x.setValue(int(self.robot_entity.transform.translation().x()))
+        self.spin_x.valueChanged.connect(self.robot_entity.setXTranslation)
+        layout.addWidget(label_x, row, 0)
+        layout.addWidget(self.spin_x, row, 1)
+        row += 1
+
+        label_y = QtWidgets.QLabel("Y")
+        self.spin_y = QtWidgets.QSpinBox()
+        self.spin_y.setStepType(QtWidgets.QAbstractSpinBox.AdaptiveDecimalStepType)
+        self.spin_y.setMinimum(0)
+        self.spin_y.setMaximum(2000)
+        self.spin_y.setValue(int(self.robot_entity.transform.translation().y()))
+        self.spin_y.valueChanged.connect(self.robot_entity.setYTranslation)
+        layout.addWidget(label_y, row, 0)
+        layout.addWidget(self.spin_y, row, 1)
+        row += 1
+
+        label_rotation = QtWidgets.QLabel("Rotation")
+        self.spin_rotation = QtWidgets.QSpinBox()
+        self.spin_rotation.setSuffix("°")
+        self.spin_rotation.setMinimum(-180)
+        self.spin_rotation.setMaximum(180)
+        self.spin_rotation.setValue(int(self.robot_entity.transform.rotationZ()))
+        self.spin_rotation.valueChanged.connect(self.robot_entity.transform.setRotationZ)
+        layout.addWidget(label_rotation, row, 0)
+        layout.addWidget(self.spin_rotation, row, 1)
+        row += 1
+
+    @classmethod
+    def set_saved_geometry(cls, geometry: QtCore.QRect):
+        """
+        Class method.
+
+        Save the position of the last displayed property window.
+
+        Arguments:
+            geometry: The position to save
+        """
+        cls.saved_geometry = geometry
+
+    @classmethod
+    def set_active_properties(cls, properties: "RobotManualProperties"):
+        """
+        Class method.
+
+        Set the current property window displayed.
+
+        Arguments:
+            properties: The current property
+        """
+        cls.active_properties = properties
+
+    def restore_saved_geometry(self):
+        """
+        Reuse the position of the last displayed property window for the current window.
+        """
+        if RobotManualProperties.saved_geometry:
+            self.setGeometry(RobotManualProperties.saved_geometry)
+
+    def closeEvent(self, event: QtGui.QCloseEvent):
+        """
+        Close the property windows.
+
+        Arguments:
+            event: The close event (unused)
+        """
+        RobotManualProperties.set_saved_geometry(self.geometry())
+        RobotManualProperties.set_active_properties(None)
+        event.accept()
