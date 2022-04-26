5deed45e0d795e00c85aebe47887cfa761284910
Starbeuck <keroullasolenn@gmail.com>
2022-04-26 07:03:23 +0200

    tools: copilot: fix cpu usage of browser when camera is display in dashboard
    Signed-off-by: Starbeuck <keroullasolenn@gmail.com>

--- cogip/tools/copilot/static/js/cameraModule.js
+++ cogip/tools/copilot/static/js/cameraModule.js
@@ -1,13 +1,14 @@
-const streaming = $(
-  `<img style="width: 100%" src="http://${window.location.hostname}:8081/"></img>`
-);
-
 let cameraDom = document.getElementById("cameraModal");
 
 window.modalCamera = function () {
+  const streaming = $(
+    `<img id="streaming" style="width: 100%" loading="lazy" src="http://${window.location.hostname}:8081/"></img>`
+  );
+
   $("#displayFlux").append(streaming);
 };
 
 cameraDom.addEventListener("hide.bs.modal", function () {
-  streaming.detach();
+  $("#streaming").attr("src", "");
+  $("#streaming").remove();
 });
