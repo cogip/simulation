c8e8cf0738a6d59fbdcc30a9a2a1f35a7beb188a
Eric Courtois <eric.courtois@gmail.com>
2022-11-01 18:44:42 +0100

    utils: add meta-class to easily create singleton classes
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

---
+++ cogip/utils/singleton.py
@@ -0,0 +1,27 @@
+class Singleton(type):
+    """
+    Meta class used to declare singleton
+
+    Example:
+
+        from dataclasses import dataclass
+
+
+        @dataclass
+        class Data(metaclass=Singleton):
+            name: str = ""
+
+
+        data = Data()
+        print(data)            # output: Data(name='')
+        data.name = "foo"
+        print(data)            # output: Data(name='foo')
+        print(Data())          # output: Data(name='foo')
+        print(data is Data())  # output: True
+    """
+    _instance = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instance:
+            cls._instance[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instance[cls]
