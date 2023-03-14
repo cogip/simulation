6cb274af127338f503f55698ea9aac98c9a2c3d3
Eric Courtois <eric.courtois@gmail.com>
2023-03-14 21:56:38 +0100

    tools: planner: avoidance: optimize visibility road map
    Compute expansion points at obstacle creation to create them only once for
    fixed obstacles.
    Filter out way points located outside of the table.
    
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- cogip/tools/planner/avoidance/avoidance.py
+++ cogip/tools/planner/avoidance/avoidance.py
@@ -62,15 +62,24 @@ def get_path(
 
 class VisibilityRoadMapWrapper(metaclass=Singleton):
     def __init__(self):
-        # Convert fixed obstacles
         self.fixed_obstacles: list[visibility_road_map.ObstaclePolygon] = []
-        x_list, y_list = list(zip(*[(int(v.x), int(v.y)) for v in borders]))
-        self.fixed_obstacles.append(visibility_road_map.ObstaclePolygon(list(x_list), list(y_list)))
         for obstacle in fixed_obstacles:
             x_list, y_list = list(zip(*[(int(v.x), int(v.y)) for v in obstacle]))
-            self.fixed_obstacles.append(visibility_road_map.ObstaclePolygon(list(x_list), list(y_list)))
+            x_list = list(x_list)
+            y_list = list(y_list)
+            x_list.append(x_list[0])
+            y_list.append(y_list[0])
+            self.fixed_obstacles.append(visibility_road_map.ObstaclePolygon(x_list, y_list, robot_width))
 
         self.expand_distance = robot_width / 2
+
+        self.visibility_road_map = visibility_road_map.VisibilityRoadMap(
+            x_min=robot_width / 2,
+            x_max=3000 - robot_width / 2,
+            y_min=-1000 + robot_width / 2,
+            y_max=1000 - robot_width / 2,
+            fixed_obstacles=self.fixed_obstacles
+        )
 
     def get_path(
             self,
@@ -82,10 +91,18 @@ class VisibilityRoadMapWrapper(metaclass=Singleton):
             if not isinstance(obstacle, models.DynRoundObstacle):
                 continue
             x_list, y_list = list(zip(*[(int(v.x), int(v.y)) for v in obstacle.bb]))
-            converted_obstacles.append(visibility_road_map.ObstaclePolygon(list(x_list), list(y_list)))
+            x_list = list(x_list)
+            y_list = list(y_list)
+            x_list.append(x_list[0])
+            y_list.append(y_list[0])
+            converted_obstacles.append(visibility_road_map.ObstaclePolygon(
+                x_list,
+                y_list,
+                robot_width * 2
+            ))
 
         # Compute path
-        rx, ry = visibility_road_map.VisibilityRoadMap(self.expand_distance).planning(
+        rx, ry = self.visibility_road_map.planning(
             start.x, start.y,
             goal.x, goal.y, converted_obstacles)
 
