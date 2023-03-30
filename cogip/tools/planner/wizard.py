1d9da132e4987ba37bebd1c6e4974b2f5019dec7
Eric Courtois <eric.courtois@gmail.com>
2023-03-30 19:39:40 +0200

    tools: planner: wizard: add starter check
    At the end of the game wizard, a first step checks that starters are
    inserted.
    The last step is waiting for started to be pulled to start the game.
    
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- cogip/tools/planner/wizard.py
+++ cogip/tools/planner/wizard.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+from cogip.utils import ThreadLoop
 from . import planner
 from .avoidance.avoidance import AvoidanceStrategy
 from .camp import Camp
@@ -14,14 +15,20 @@ class GameWizard:
         self.step = 0
         self.robot_index = 0
         self.started = False
+        self.waiting_start_thread = ThreadLoop(
+            "Waiting start thread",
+            0.1,
+            self.check_start,
+            logger=True
+        )
 
         self.steps = [
             (self.request_camp, self.response_camp),
             (self.request_start_pose, self.response_start_pose),
             (self.request_strategy, self.response_strategy),
             (self.request_avoidance, self.response_avoidance),
-            # (self.request_starter, self.response_starter),
-            (self.request_play, self.response_play)
+            (self.request_starter, self.response_starter),
+            (self.request_wait, self.response_wait)
         ]
 
     def reset(self):
@@ -121,31 +128,62 @@ class GameWizard:
         self.game_context.avoidance_strategy = new_strategy
         self.reset()
 
-    # def request_starter(self):
-    #     # TODO
-    #     pass
+    def request_starter(self):
+        if self.robot_index >= len(self.planner._robots):
+            self.waiting_start_thread.start()
+            self.robot_index = 0
+            self.next()
+            return
 
-    # def response_start(self, message: dict[str, Any]):
-    #     # TODO
-    #     pass
+        robot_id, robot = list(self.planner._robots.items())[self.robot_index]
+        if robot.starter.is_pressed:
+            self.robot_index += 1
+            self.step -= 1
+            self.next()
+            return
 
-    def request_play(self):
         message = {
-            "name": "Game Wizard: Ready?",
-            "type": "boolean",
-            "value": False
+            "name": "Game Wizard: Starter Check",
+            "type": "message",
+            "value": f"Please insert starter in Robot {robot_id}",
+            "robot_id": robot_id
         }
         self.planner._sio_ns.emit("wizard", message)
 
-    def response_play(self, message: dict[str, Any]):
-        ready = message["value"]
-        if not ready:
+    def response_starter(self, message: dict[str, Any]):
+        robot_id = message["robot_id"]
+        robot = self.planner._robots[robot_id]
+        if not robot.starter.is_pressed:
+            self.step -= 1
+            return
+
+        if self.robot_index < len(self.planner._robots) - 1:
+            self.step -= 1
+            self.robot_index += 1
+            return
+
+        self.waiting_start_thread.start()
+
+    def request_wait(self):
+        message = {
+            "name": "Game Wizard: Waiting Start",
+            "type": "message",
+            "value": f"Remove starter{'s' if len(self.planner._robots) else ''} to start the game"
+        }
+        self.planner._sio_ns.emit("wizard", message)
+
+    def response_wait(self, message: dict[str, Any]):
+        if self.started:
             self.reset()
             return
 
-        self.planner.reset()
+        self.step -= 1
 
-        # TODO: wait for starter
+    def check_start(self):
+        if not all([not robot.starter.is_pressed for robot in self.planner._robots.values()]):
+            return
+
+        self.waiting_start_thread._cancel = True
         self.started = True
         self.planner._sio_ns.emit("close_wizard")
         self.planner.reset()
