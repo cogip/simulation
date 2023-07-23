4de42a84455d587907509929b3ae8e25f7226ab9
Eric Courtois <eric.courtois@gmail.com>
2023-07-23 11:50:07 +0200

    tools: planner: integrate all remaining changes made during the cup
    There are too many remaining changes from the cup to split them
    into clean commits, so this commit integrates all of them to merge
    this branch.
    
    Here are the major changes:
    - add log messages
    - better support for multiple robots
    - add cake drop slots
    - add countdown
    - improve shares date and queues with avoidance processes
    - trigger final actions at end of countdown
    - control video recording
    
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- cogip/tools/planner/robot.py
+++ cogip/tools/planner/robot.py
@@ -1,4 +1,7 @@
+import asyncio
+from multiprocessing.managers import Namespace
 from typing import TYPE_CHECKING
+
 from functools import partial
 from gpiozero import Button
 from gpiozero.pins.mock import MockFactory
@@ -6,7 +9,8 @@ from gpiozero.pins.pigpio import PiGPIOFactory
 
 from cogip.models import models
 from cogip.tools.copilot.controller import ControllerEnum
-from . import actions, context, logger, pose
+from .actions import actions
+from . import cake, context, logger, pose
 from .strategy import Strategy
 
 if TYPE_CHECKING:
@@ -18,7 +22,9 @@ class Robot:
     Class representing a robot context.
     """
     def __init__(self, robot_id: int, planner: "Planner", virtual: bool):
+        self.namespace: Namespace = planner._process_manager.Namespace()
         self.robot_id = robot_id
+        self.namespace.robot_id = robot_id
         self.planner = planner
         self.virtual = virtual
         self.game_context = context.GameContext()
@@ -29,13 +35,14 @@ class Robot:
         self.avoidance_path: list[pose.Pose] = []
         self.last_avoidance_pose_current: models.Pose | None = None
         self.last_emitted_pose_order: models.PathPose | None = None
-        self._controller: ControllerEnum = self.game_context.default_controller
-        controllers = self.planner._shared_properties["controllers"]
-        controllers[robot_id] = self._controller
-        self.planner._shared_properties["controllers"] = controllers
+        self.controller: ControllerEnum | None = None
         self._obstacles: models.DynObstacleList = []
         self.starter: Button | None = None
         self.blocked: int = 0
+        self.cake: "cake.Cake" | None = None
+        self.nb_cherries = 5
+        self.parked = False
+        self.sio_receiver_queue = asyncio.Queue()
 
         if virtual:
             starter = Button(
@@ -53,8 +60,8 @@ class Robot:
                 pin_factory=PiGPIOFactory(host=f"robot{robot_id}")
             )
 
-        starter.when_pressed = partial(self.sio_emitter_queue.put, ("starter_changed", True))
-        starter.when_released = partial(self.sio_emitter_queue.put, ("starter_changed", False))
+        starter.when_pressed = partial(planner._sio_emitter_queues[robot_id].put, ("starter_changed", True))
+        starter.when_released = partial(planner._sio_emitter_queues[robot_id].put, ("starter_changed", False))
         self.starter = starter
 
     async def set_pose_start(self, pose: models.Pose):
@@ -67,21 +74,23 @@ class Robot:
         self.pose_reached = True
         self.avoidance_path = []
         await self.planner.set_pose_start(self.robot_id, self.pose_current)
-        # await self.pose_order.act_after_pose(self.planner)
 
     async def set_pose_reached(self, reached: bool = True):
         """
         Set pose reached.
         If reached, a new pose and new action is selected.
         """
-        if reached and not self.pose_reached and self.pose_order:
-            await self.pose_order.act_after_pose(self.planner)
-        self.pose_reached = reached
-        self.pose_order = None
+        logger.debug(f"Robot {self.robot_id}: set_pose_reached({reached})")
         self.avoidance_path = []
-        if self.action and len(self.action.poses) == 0:
-            await self.action.act_after_action(self.planner)
+        if reached and not self.pose_reached and (pose_order := self.pose_order):
+            self.pose_order = None
+            await pose_order.act_after_pose()
+        else:
+            self.pose_order = None
+        self.pose_reached = reached
+        if (action := self.action) and len(self.action.poses) == 0:
             self.action = None
+            await action.act_after_action()
 
     @property
     def pose_current(self) -> models.Pose:
@@ -103,17 +112,15 @@ class Robot:
             del self.planner._shared_poses_order[self.robot_id]
         elif new_pose is not None:
             self.planner._shared_poses_order[self.robot_id] = new_pose.path_pose.dict(exclude_unset=True)
+            if self.robot_id in self.planner._shared_last_avoidance_pose_currents:
+                del self.planner._shared_last_avoidance_pose_currents[self.robot_id]
 
-    @property
-    def controller(self) -> ControllerEnum:
-        return self._controller
-
-    @controller.setter
-    def controller(self, new_controller: ControllerEnum):
-        self._controller = new_controller
-        controllers = self.planner._shared_properties["controllers"]
-        controllers[self.robot_id] = new_controller
-        self.planner._shared_properties["controllers"] = controllers
+    async def set_controller(self, new_controller: ControllerEnum):
+        if self.controller == new_controller:
+            return
+        self.controller = new_controller
+        self.namespace.controller = new_controller.value
+        await self.planner._sio_ns.emit("set_controller", (self.robot_id, self.controller.value))
 
     @property
     def obstacles(self) -> models.DynObstacleList:
@@ -132,14 +139,17 @@ class Robot:
         Select next pose.
         Returns None if no more pose is available in the current action.
         """
+        logger.debug(f"Robot {self.robot_id}: next_pose()")
         self.pose_reached = False
-        if not self.action or len(self.action.poses) == 0:
-            self.action = None
+        if not self.action:
+            return None
+
+        if len(self.action.poses) == 0:
             return None
 
         pose_order = self.action.poses.pop(0)
         self.pose_order = None
-        await pose_order.act_before_pose(self.planner)
+        await pose_order.act_before_pose()
         self.blocked = 0
         self.pose_order = pose_order
 
@@ -148,11 +158,12 @@ class Robot:
 
         return self.pose_order
 
-    async def set_action(self, action: "actions.Action"):
+    async def set_action(self, action: "actions.Action") -> pose.Pose | None:
         """
         Set current action.
         """
         logger.debug(f"Robot {self.robot_id}: set action '{action.name}'")
+        self.pose_order = None
         self.action = action
-        await self.action.act_before_action(self.planner)
-        await self.next_pose()
+        await self.action.act_before_action()
+        return await self.next_pose()
