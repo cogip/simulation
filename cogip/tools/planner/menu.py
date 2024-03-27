c7291cbb0fe76670218c8714a67ac7b1a676e38c
Gilles DOFFE <gilles.doffe@rtone.fr>
2024-03-27 20:45:16 +0100

    WIP 2

--- cogip/tools/planner/menu.py
+++ cogip/tools/planner/menu.py
@@ -45,25 +45,7 @@ cameras_menu = models.ShellMenu(
     ],
 )
 
-actuators_commands = [
-    "pump_left_on",
-    "pump_left_off",
-    "pump_right_on",
-    "pump_right_off",
-    "left_arm_folded",
-    "left_arm_extended",
-    "right_arm_folded",
-    "right_arm_extended",
-    "right_arm_mid",
-    "led_off",
-    "led_on",
-    "central_arm_up",
-    "central_arm_down",
-    "left_arm_up",
-    "left_arm_down",
-    "right_arm_up",
-    "right_arm_down",
-]
+actuators_commands = ["analogservo_bottom_arm_left_close", "analogservo_bottom_arm_left_open"]
 
 actuators_menu_1 = models.ShellMenu(
     name="Actuators 1",
@@ -76,40 +58,5 @@ actuators_menu_2 = models.ShellMenu(
     name="Actuators 2",
     entries=[
         models.MenuEntry(cmd=f"act_{cmd}_2", desc=f"{cmd.replace('_', ' ').title()}") for cmd in actuators_commands
-    ],
-)
-
-
-cherries_commands = [
-    "action_deliver_on_cake",
-    "action_launch_start",
-    "action_launch_stop",
-    "action_aspirate_start",
-    "action_aspirate_stop",
-    "cherry_switch_closed",
-    "cherry_switch_cake",
-    "cherry_switch_launcher",
-    "cherry_arm_up",
-    "cherry_arm_down",
-    "cherry_esc_off",
-    "cherry_esc_on",
-    "cherry_esc_eject",
-    "cherry_release_down",
-    "cherry_release_up",
-    "cherry_conveyor_on",
-    "cherry_conveyor_off",
-]
-
-cherries_menu_1 = models.ShellMenu(
-    name="Cherries 1",
-    entries=[
-        models.MenuEntry(cmd=f"act_{cmd}_1", desc=f"{cmd.replace('_', ' ').title()}") for cmd in cherries_commands
-    ],
-)
-
-cherries_menu_2 = models.ShellMenu(
-    name="Cherries 2",
-    entries=[
-        models.MenuEntry(cmd=f"act_{cmd}_2", desc=f"{cmd.replace('_', ' ').title()}") for cmd in cherries_commands
     ],
 )
--- deploy_robot2.sh
+++ deploy_robot2.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-python setup.py sdist && scp dist/cogip-tools-1.0.0.tar.gz robot2: && ssh robot2 sudo python3 -m pip install cogip-tools-1.0.0.tar.gz && ssh robot2 sudo systemctl restart copilot detector robotcam
+python setup.py sdist
+scp dist/cogip-tools-1.0.0.tar.gz robot2:
+ssh robot2 sudo python3 -m pip install cogip-tools-1.0.0.tar.gz
+ssh robot2 sudo systemctl restart cogip-copilot cogip-server cogip-planner cogip-dashboard
