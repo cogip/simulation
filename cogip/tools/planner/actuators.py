9d480e33b56545bcb2965350d43787f32c34f474
Eric Courtois <eric.courtois@gmail.com>
2024-05-05 19:18:11 +0200

    tools: planner: set actuators values after calibration
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- cogip/tools/planner/actuators.py
+++ cogip/tools/planner/actuators.py
@@ -58,7 +58,7 @@ async def bottom_lift_down(planner: "Planner") -> float:
 
 
 async def bottom_lift_up(planner: "Planner") -> float:
-    await positional_motor_command(planner, PositionalActuatorEnum.MOTOR_BOTTOM_LIFT, 1)
+    await positional_motor_command(planner, PositionalActuatorEnum.MOTOR_BOTTOM_LIFT, 76)
     return 0
 
 
@@ -67,51 +67,191 @@ async def top_lift_down(planner: "Planner") -> float:
     return 0
 
 
-async def top_lift_up(planner: "Planner") -> float:
-    await positional_motor_command(planner, PositionalActuatorEnum.MOTOR_TOP_LIFT, 1)
+async def top_lift_mid(planner: "Planner") -> float:
+    await positional_motor_command(planner, PositionalActuatorEnum.MOTOR_TOP_LIFT, 86)
     return 0
 
 
+async def top_lift_up(planner: "Planner") -> float:
+    await positional_motor_command(planner, PositionalActuatorEnum.MOTOR_TOP_LIFT, 165)
+    return 0
+
+
+# Bottom grip left
+
+bottom_grip_left_closed_val = 235
+bottom_grip_left_open_val = 147
+bottom_grip_left_incr = int((bottom_grip_left_open_val - bottom_grip_left_closed_val) / 4)
+
+
 async def bottom_grip_left_close(planner: "Planner") -> float:
-    await positional_motor_command(planner, PositionalActuatorEnum.ANALOGSERVO_BOTTOM_GRIP_LEFT, 0)
+    await positional_motor_command(
+        planner, PositionalActuatorEnum.ANALOGSERVO_BOTTOM_GRIP_LEFT, bottom_grip_left_closed_val
+    )
+    return 0
+
+
+async def bottom_grip_left_mid_close(planner: "Planner") -> float:
+    await positional_motor_command(
+        planner,
+        PositionalActuatorEnum.ANALOGSERVO_BOTTOM_GRIP_LEFT,
+        bottom_grip_left_closed_val + bottom_grip_left_incr,
+    )
+    return 0
+
+
+async def bottom_grip_left_mid(planner: "Planner") -> float:
+    await positional_motor_command(
+        planner,
+        PositionalActuatorEnum.ANALOGSERVO_BOTTOM_GRIP_LEFT,
+        bottom_grip_left_closed_val + 2 * bottom_grip_left_incr,
+    )
+    return 0
+
+
+async def bottom_grip_left_mid_open(planner: "Planner") -> float:
+    await positional_motor_command(
+        planner,
+        PositionalActuatorEnum.ANALOGSERVO_BOTTOM_GRIP_LEFT,
+        bottom_grip_left_closed_val + 3 * bottom_grip_left_incr,
+    )
     return 0
 
 
 async def bottom_grip_left_open(planner: "Planner") -> float:
-    await positional_motor_command(planner, PositionalActuatorEnum.ANALOGSERVO_BOTTOM_GRIP_LEFT, 1)
+    await positional_motor_command(
+        planner, PositionalActuatorEnum.ANALOGSERVO_BOTTOM_GRIP_LEFT, bottom_grip_left_open_val
+    )
     return 0
 
 
+# Bottom grip right
+
+bottom_grip_right_closed_val = 154
+bottom_grip_right_open_val = 235
+bottom_grip_right_incr = int((bottom_grip_right_open_val - bottom_grip_right_closed_val) / 4)
+
+
 async def bottom_grip_right_close(planner: "Planner") -> float:
-    await positional_motor_command(planner, PositionalActuatorEnum.ANALOGSERVO_BOTTOM_GRIP_RIGHT, 0)
+    await positional_motor_command(
+        planner, PositionalActuatorEnum.ANALOGSERVO_BOTTOM_GRIP_RIGHT, bottom_grip_right_closed_val
+    )
+    return 0
+
+
+async def bottom_grip_right_mid_close(planner: "Planner") -> float:
+    await positional_motor_command(
+        planner,
+        PositionalActuatorEnum.ANALOGSERVO_BOTTOM_GRIP_RIGHT,
+        bottom_grip_right_closed_val + bottom_grip_right_incr,
+    )
+    return 0
+
+
+async def bottom_grip_right_mid(planner: "Planner") -> float:
+    await positional_motor_command(
+        planner,
+        PositionalActuatorEnum.ANALOGSERVO_BOTTOM_GRIP_RIGHT,
+        bottom_grip_right_closed_val + 2 * bottom_grip_right_incr,
+    )
+    return 0
+
+
+async def bottom_grip_right_mid_open(planner: "Planner") -> float:
+    await positional_motor_command(
+        planner,
+        PositionalActuatorEnum.ANALOGSERVO_BOTTOM_GRIP_RIGHT,
+        bottom_grip_right_closed_val + 3 * bottom_grip_right_incr,
+    )
     return 0
 
 
 async def bottom_grip_right_open(planner: "Planner") -> float:
-    await positional_motor_command(planner, PositionalActuatorEnum.ANALOGSERVO_BOTTOM_GRIP_RIGHT, 1)
+    await positional_motor_command(
+        planner, PositionalActuatorEnum.ANALOGSERVO_BOTTOM_GRIP_RIGHT, bottom_grip_right_open_val
+    )
     return 0
 
 
+# Top grip left
+
+top_grip_left_closed_val = 228
+top_grip_left_open_val = 132
+top_grip_left_incr = int((top_grip_left_open_val - top_grip_left_closed_val) / 4)
+
+
 async def top_grip_left_close(planner: "Planner") -> float:
-    await positional_motor_command(planner, PositionalActuatorEnum.ANALOGSERVO_TOP_GRIP_LEFT, 0)
+    await positional_motor_command(planner, PositionalActuatorEnum.ANALOGSERVO_TOP_GRIP_LEFT, top_grip_left_closed_val)
+    return 0
+
+
+async def top_grip_left_mid_close(planner: "Planner") -> float:
+    await positional_motor_command(
+        planner, PositionalActuatorEnum.ANALOGSERVO_TOP_GRIP_LEFT, top_grip_left_closed_val + top_grip_left_incr
+    )
+    return 0
+
+
+async def top_grip_left_mid(planner: "Planner") -> float:
+    await positional_motor_command(
+        planner, PositionalActuatorEnum.ANALOGSERVO_TOP_GRIP_LEFT, top_grip_left_closed_val + 2 * top_grip_left_incr
+    )
+    return 0
+
+
+async def top_grip_left_mid_open(planner: "Planner") -> float:
+    await positional_motor_command(
+        planner, PositionalActuatorEnum.ANALOGSERVO_TOP_GRIP_LEFT, top_grip_left_closed_val + 3 * top_grip_left_incr
+    )
     return 0
 
 
 async def top_grip_left_open(planner: "Planner") -> float:
-    await positional_motor_command(planner, PositionalActuatorEnum.ANALOGSERVO_TOP_GRIP_LEFT, 1)
+    await positional_motor_command(planner, PositionalActuatorEnum.ANALOGSERVO_TOP_GRIP_LEFT, top_grip_left_open_val)
     return 0
 
 
+# Top grip right
+
+top_grip_right_closed_val = 140
+top_grip_right_open_val = 226
+top_grip_right_incr = int((top_grip_right_open_val - top_grip_right_closed_val) / 4)
+
+
 async def top_grip_right_close(planner: "Planner") -> float:
-    await positional_motor_command(planner, PositionalActuatorEnum.ANALOGSERVO_TOP_GRIP_RIGHT, 0)
+    await positional_motor_command(
+        planner, PositionalActuatorEnum.ANALOGSERVO_TOP_GRIP_RIGHT, top_grip_right_closed_val
+    )
+    return 0
+
+
+async def top_grip_right_mid_close(planner: "Planner") -> float:
+    await positional_motor_command(
+        planner, PositionalActuatorEnum.ANALOGSERVO_TOP_GRIP_RIGHT, top_grip_right_closed_val + top_grip_right_incr
+    )
+    return 0
+
+
+async def top_grip_right_mid(planner: "Planner") -> float:
+    await positional_motor_command(
+        planner, PositionalActuatorEnum.ANALOGSERVO_TOP_GRIP_RIGHT, top_grip_right_closed_val + 2 * top_grip_right_incr
+    )
+    return 0
+
+
+async def top_grip_right_mid_open(planner: "Planner") -> float:
+    await positional_motor_command(
+        planner, PositionalActuatorEnum.ANALOGSERVO_TOP_GRIP_RIGHT, top_grip_right_closed_val + 3 * top_grip_right_incr
+    )
     return 0
 
 
 async def top_grip_right_open(planner: "Planner") -> float:
-    await positional_motor_command(planner, PositionalActuatorEnum.ANALOGSERVO_TOP_GRIP_RIGHT, 1)
+    await positional_motor_command(planner, PositionalActuatorEnum.ANALOGSERVO_TOP_GRIP_RIGHT, top_grip_right_open_val)
     return 0
 
 
+# Cart magnets
 async def cart_magnet_left_on(planner: "Planner") -> float:
     await positional_motor_command(planner, PositionalActuatorEnum.CART_MAGNET_LEFT, 0)
     return 0
@@ -143,10 +283,37 @@ async def pami_arm_open(planner: "Planner") -> float:
 
 
 # Combined actions
+
+
+# Bottom grip
 async def bottom_grip_close(planner: "Planner") -> float:
     await asyncio.gather(
         bottom_grip_left_close(planner),
         bottom_grip_right_close(planner),
+    )
+    return 0
+
+
+async def bottom_grip_mid_close(planner: "Planner") -> float:
+    await asyncio.gather(
+        bottom_grip_left_mid_close(planner),
+        bottom_grip_right_mid_close(planner),
+    )
+    return 0
+
+
+async def bottom_grip_mid(planner: "Planner") -> float:
+    await asyncio.gather(
+        bottom_grip_left_mid(planner),
+        bottom_grip_right_mid(planner),
+    )
+    return 0
+
+
+async def bottom_grip_mid_open(planner: "Planner") -> float:
+    await asyncio.gather(
+        bottom_grip_left_mid_open(planner),
+        bottom_grip_right_mid_open(planner),
     )
     return 0
 
@@ -159,10 +326,37 @@ async def bottom_grip_open(planner: "Planner") -> float:
     return 0
 
 
+# Top grip
+
+
 async def top_grip_close(planner: "Planner") -> float:
     await asyncio.gather(
         top_grip_left_close(planner),
         top_grip_right_close(planner),
+    )
+    return 0
+
+
+async def top_grip_mid_close(planner: "Planner") -> float:
+    await asyncio.gather(
+        top_grip_left_mid_close(planner),
+        top_grip_right_mid_close(planner),
+    )
+    return 0
+
+
+async def top_grip_mid(planner: "Planner") -> float:
+    await asyncio.gather(
+        top_grip_left_mid(planner),
+        top_grip_right_mid(planner),
+    )
+    return 0
+
+
+async def top_grip_mid_open(planner: "Planner") -> float:
+    await asyncio.gather(
+        top_grip_left_mid_open(planner),
+        top_grip_right_mid_open(planner),
     )
     return 0
 
@@ -175,6 +369,7 @@ async def top_grip_open(planner: "Planner") -> float:
     return 0
 
 
+# Cart
 async def cart_in(planner: "Planner") -> float:
     await asyncio.gather(
         left_cart_in(planner),
@@ -191,6 +386,7 @@ async def cart_out(planner: "Planner") -> float:
     return 0
 
 
+# Cart magnets
 async def cart_magnet_on(planner: "Planner") -> float:
     await asyncio.gather(
         cart_magnet_left_on(planner),
