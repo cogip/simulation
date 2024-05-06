9a4682821f50e6246fd81ac8e3eb40cf0bb7b643
Eric Courtois <eric.courtois@gmail.com>
2024-05-06 17:30:30 +0200

    tools: planner: add Training strategy for training table
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- cogip/tools/planner/actions/training.py
+++ cogip/tools/planner/actions/training.py
@@ -1,42 +1,31 @@
 from typing import TYPE_CHECKING
 
-from cogip.models import models
+from cogip.models import artifacts, models
 from .. import table
-from ..pose import Pose
-from .actions import Action, Actions, WaitAction
+from ..positions import StartPosition
+from . import base_actions
+from .actions import Actions, WaitAction
 
 if TYPE_CHECKING:
     from ..planner import Planner
-
-
-class FinalAction(Action):
-    def __init__(self, planner: "Planner", actions: Actions, pose: models.Pose):
-        super().__init__(f"Final action at ({int(pose.x)}, {int(pose.y)})", planner, actions)
-        self.pose = Pose(**pose.model_dump(), after_pose_func=self.after_final)
-        self.poses = [self.pose]
-
-    def weight(self) -> float:
-        if self.game_context.countdown > 10:
-            return 0
-        return 100000
-
-    async def after_final(self):
-        self.actions.clear()
-
-        self.game_context.score += 15
-
-        await self.planner.sio_ns.emit("score", self.game_context.score)
 
 
 class TrainingActions(Actions):
     def __init__(self, planner: "Planner"):
         super().__init__(planner)
 
-        self.planner.start_position = 2
-        self.append(WaitAction(planner, self))
-        self.append(FinalAction(planner, self, models.Pose(x=-500, y=-500, O=180)))
+        if self.game_context._table == table.TableEnum.Training:
+            parking_pose = models.Pose(x=-1000 + 450, y=-1500 + 450, O=0)
+            planter_id = artifacts.PlanterID.Test
+        else:
+            parking_pose = models.Pose(x=0, y=1500 + 450, O=0)
+            planter_id = artifacts.PlanterID.Top
 
-        if self.game_context._table != table.TableEnum.Training:
-            self.planner.start_position = 1
-            self.append(WaitAction(planner, self))
-            self.append(FinalAction(planner, self, models.Pose(x=-500, y=500, O=180)))
+        self.planner.start_position = StartPosition.Bottom
+        self.append(base_actions.SolarPanelsAction(planner, self, artifacts.SolarPanelsID.Local))
+        self.append(base_actions.GripAction(planner, self, artifacts.PlantSupplyID.LocalBottom))
+        self.append(base_actions.PotCaptureAction(planner, self, artifacts.PotSupplyID.LocalMiddle))
+        self.append(base_actions.DropInDropoffZoneAction(planner, self, artifacts.DropoffZoneID.Bottom, 2))
+        self.append(base_actions.DropInPlanterAction(planner, self, planter_id))
+        self.append(WaitAction(planner, self))
+        self.append(base_actions.ParkingAction(planner, self, parking_pose))
