9f74ae1e92a6fcd920a7e0ec00285160b58b85f8
Eric Courtois <eric.courtois@gmail.com>
2024-06-11 20:50:11 +0200

    raspios: install devpi-server on beacon and use it as proxy on all devices
    The devpi-server on the beacon is used as a pypi cache to allow
    installation of COGIP tools on beacon and robots even without internet
    connection.
    
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- raspios/stage1_customize_image.sh
+++ raspios/stage1_customize_image.sh
@@ -4,11 +4,11 @@ SCRIPT_DIR=`dirname $SCRIPT`
 source ${SCRIPT_DIR}/common.sh
 
 # Generate COGIP tools package
-(cd .. && python3 setup.py sdist --dist-dir ${SCRIPT_DIR})
+(cd .. && python3 setup.py sdist --formats=gztar --dist-dir ${SCRIPT_DIR})
 cp -f ../requirements.txt .
 
 DOCKER_BUILDKIT=1 docker build --progress plain . -f Dockerfile.customize --target beacon -t cogip/raspios:beacon
 DOCKER_BUILDKIT=1 docker build --progress plain . -f Dockerfile.customize --target robot -t cogip/raspios:robot
 DOCKER_BUILDKIT=1 docker build --progress plain . -f Dockerfile.customize --target pami -t cogip/raspios:pami
 
-rm -f requirements.txt
+rm -f cogip_tools-*.tar.gz requirements.txt
